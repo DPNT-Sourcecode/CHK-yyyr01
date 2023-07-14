33379ab397ba6572dad4e0dec582f2ae1c043ec7
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 15:11:05 +0000

    Fri Jul 14 15:11:05 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,9 +1,12 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+    if type(x) != int or type(y) != int:
+        raise Exception("number should be a valid integer between 0 and 100")
     if (x > 100 or x < 0) or ( y > 100 or y < 0):
         raise Exception("number should be between 0 and 100")
-    if (x > 100 or x < 0) or ( y > 100 or y < 0):
-        raise Exception("number should be between 0 and 100")
+    
     return x + y
+
+print(compute(3,500))
 
 
