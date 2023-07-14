b17ea16496f6d1aea7b444a6dd511d154a2e99a4
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 18:39:08 +0000

    Fri Jul 14 18:39:08 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -47,14 +47,17 @@ def checkout(skus):
                 offer, leftover = divmod(quantity, 5)
                 offer_price += quantity * 200 + leftover * skus_dict[item]["price"]
             total_price += offer_price
-        if item == "B":
+        elif item == "B":
             offer_price = 0
             quantity = item_details[item]
             if quantity >= 2:
                 offer, leftover = divmod(quantity, 5)
                 offer_price += quantity * 45 + leftover * skus_dict[item]["price"]
             total_price += offer_price
-
+        else:
+            total_price += item_details[item] * skus_dict[item]
+    
+    return total_price
                 
                 
 
@@ -89,46 +92,47 @@ def checkout(skus):
     return total_checkout_value
 
 
-def chk_r1_checkout(skus):
-    # Initialize total checkout value to zero
-    total_checkout_value = 0
-    #  Store items with offers and quantity purchased
-    offer_check = {}
-    # loop through each skus to get their price
-    for item in skus:
-        # Check for invalid entry and return -1
-        try:
-            item_details = skus_dict[item]
-        except KeyError:
-            return -1
-        # Check if there is offer on item
-        offer_details = item_details.get("offers")
-        #  Process offer on item if any
-        if item == "A":
+# def chk_r1_checkout(skus):
+    # # Initialize total checkout value to zero
+    # total_checkout_value = 0
+    # #  Store items with offers and quantity purchased
+    # offer_check = {}
+    # # loop through each skus to get their price
+    # for item in skus:
+    #     # Check for invalid entry and return -1
+    #     try:
+    #         item_details = skus_dict[item]
+    #     except KeyError:
+    #         return -1
+    #     # Check if there is offer on item
+    #     offer_details = item_details.get("offers")
+    #     #  Process offer on item if any
+    #     if item == "A":
 
-            # # Split out offer details
-            # offer = offer_details.split(" ")
-            # offer_quantity = int(offer[0][0])
-            # offer_price = int(offer[2])
-            # if item not in offer_check:
-            #     offer_check[item] = 1
-            # else:
-            #     offer_check[item] += 1
-            #     # Process if quantity is up to offer quantity
-            #     if offer_check[item] == offer_quantity:
-            #         total_checkout_value += offer_price
-            #         del offer_check[item]
-        # Add each sku price to total checkout
-        else:
-            total_checkout_value += skus_dict[item]["price"]
+    #         # # Split out offer details
+    #         # offer = offer_details.split(" ")
+    #         # offer_quantity = int(offer[0][0])
+    #         # offer_price = int(offer[2])
+    #         # if item not in offer_check:
+    #         #     offer_check[item] = 1
+    #         # else:
+    #         #     offer_check[item] += 1
+    #         #     # Process if quantity is up to offer quantity
+    #         #     if offer_check[item] == offer_quantity:
+    #         #         total_checkout_value += offer_price
+    #         #         del offer_check[item]
+    #     # Add each sku price to total checkout
+    #     else:
+    #         total_checkout_value += skus_dict[item]["price"]
     
-    # Process items not up to offer
-    for offer in offer_check:
-        item = skus_dict[offer]
-        total_checkout_value += offer_check[offer] * item["price"]
+    # # Process items not up to offer
+    # for offer in offer_check:
+    #     item = skus_dict[offer]
+    #     total_checkout_value += offer_check[offer] * item["price"]
         
-    #  return total checkout
-    return total_checkout_value
+    # #  return total checkout
+    # return total_checkout_value
+
 
 
 
