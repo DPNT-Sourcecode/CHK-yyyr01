561f43a8832addfafc78e2133f784438968fa821
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 19:18:08 +0000

    Fri Jul 14 19:18:08 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -32,7 +32,6 @@ def checkout(skus):
         else:
             item_details[item] += 1
     
-    print(item_details, "+++")
     total_price = 0
     for item in item_details:
         if item == "A":
@@ -40,11 +39,9 @@ def checkout(skus):
             quantity = item_details[item]
             if quantity >= 5:
                 offer, leftover = divmod(quantity, 5)
-                print(offer, leftover, "==")
                 offer_price += offer * 200
                 if leftover >= 3:
                     offer, leftover = divmod(leftover, 3)
-                    print(skus_dict[item], offer, leftover)
                     offer_price += (offer * 130) + leftover * skus_dict[item]["price"]
                 else:
                     offer_price = item_details[item] * skus_dict[item]["price"]
@@ -53,55 +50,25 @@ def checkout(skus):
                 offer_price += (offer * 130) + (leftover * skus_dict[item]["price"])
             else:
                 offer_price = item_details[item] * skus_dict[item]["price"]
-            print(offer_price, "AAA")
             total_price += offer_price
         elif item == "B":
             offer_price = 0
             quantity = item_details[item]
             if quantity >= 2:
                 offer, leftover = divmod(quantity, 2)
-                print(offer, leftover)
                 offer_price += (offer * 45) + (leftover * skus_dict[item]["price"])
             else:
                 offer_price = item_details[item] * skus_dict[item]["price"]
-            print(offer_price, "BBB")
             total_price += offer_price
+        elif item == "E":
+            quantity = item_details[item]
+            if quantity >= 2:
+                
         else:
             total_price += item_details[item] * skus_dict[item]["price"]
     
     return total_price
-                
-                
 
-        
-    
-        # # Check if there is offer on item
-        # offer_details = item_details.get("offers")
-        # #  Process offer on item if any
-        # if offer_details:
-        #     # Split out offer details
-        #     offer = offer_details.split(" ")
-        #     offer_quantity = int(offer[0][0])
-        #     offer_price = int(offer[2])
-        #     if item not in offer_check:
-        #         offer_check[item] = 1
-        #     else:
-        #         offer_check[item] += 1
-        #         # Process if quantity is up to offer quantity
-        #         if offer_check[item] == offer_quantity:
-        #             total_checkout_value += offer_price
-        #             del offer_check[item]
-        # # Add each sku price to total checkout
-        # else:
-        #     total_checkout_value += skus_dict[item]["price"]
-    
-    # Process items not up to offer
-    for offer in offer_check:
-        item = skus_dict[offer]
-        total_checkout_value += offer_check[offer] * item["price"]
-        
-    #  return total checkout
-    return total_checkout_value
 
 
 # def chk_r1_checkout(skus):
@@ -144,6 +111,7 @@ def checkout(skus):
         
     # #  return total checkout
     # return total_checkout_value
+
 
 
 
