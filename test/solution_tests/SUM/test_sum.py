0e161efd59d78dcc4163223adcaa360c4813eb15
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 15:26:05 +0000

    Fri Jul 14 15:26:05 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,14 +7,15 @@ class TestSum():
         assert sum_solution.compute(1, 2) == 3
 
     def test_params_are_integers(self):
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as err:
             sum_solution("5", [34])
     
     def test_params_are_integers_bewteen_0_100(self):
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as err:
             sum_solution(5, 101)
             sum_solution(1005, 1)
             sum_solution(1005, 101)
+
 
 
 
