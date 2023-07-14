f56307f62990d93fdb49897dd2d0fd1db18b17e4
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 19:40:28 +0000

    Fri Jul 14 19:40:28 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,20 +1,34 @@
-from lib.solutions.SUM import sum_solution
+from lib.solutions.CHK import checkout_solution
 import pytest
 
 
-class TestSum():
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+class TestCheckout():
+    def test_checkout_valid(self):
+        assert checkout_solution.checkout("A") == 50
 
-    def test_params_are_integers(self):
-        with pytest.raises(Exception) as err:
-            sum_solution("5", [34])
-    
-    def test_params_are_integers_bewteen_0_100(self):
-        with pytest.raises(Exception) as err:
-            sum_solution(5, 101)
-            sum_solution(1005, 1)
-            sum_solution(1005, 101)
+    def test_checkout_invalid(self):
+        assert checkout_solution.checkout("PA") == -1
+
+    def test_checkout_valid_multiple_without_offer(self):
+        assert checkout_solution.checkout("AA") == 100
+        assert checkout_solution.checkout("ADA") == 115
+
+    def test_checkout_valid_multiple_with_offer(self):
+        assert checkout_solution.checkout("AAAAAAA") == 300
+        assert checkout_solution.checkout("AAAAAA") == 330
+        assert checkout_solution.checkout("ADA") == 115
+        assert checkout_solution.checkout("ABAB") == 145
+        assert checkout_solution.checkout("AABAB") == 175
+        assert checkout_solution.checkout("AAAAAAAA") == 330
+
+    def test_checkout_get_free_B_with_offer(self):
+        assert checkout_solution.checkout("EEB") == 80
+        assert checkout_solution.checkout("EEBB") == 110
+        assert checkout_solution.checkout("EABB") == 135
+        assert checkout_solution.checkout("EBB") == 85
+        assert checkout_solution.checkout("EABD") == 135
+
+
 
 
 
