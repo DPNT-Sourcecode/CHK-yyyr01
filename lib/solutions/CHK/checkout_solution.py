cb11cd544fd08270141fa7eae34ffc384733df8e
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 18:15:08 +0000

    Fri Jul 14 18:15:08 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -54,3 +54,45 @@ def checkout(skus):
     return total_checkout_value
 
 
+def chk_r1_checkout(skus):
+    # Initialize total checkout value to zero
+    total_checkout_value = 0
+    #  Store items with offers and quantity purchased
+    offer_check = {}
+    # loop through each skus to get their price
+    for item in skus:
+        # Check for invalid entry and return -1
+        try:
+            item_details = skus_dict[item]
+        except KeyError:
+            return -1
+        # Check if there is offer on item
+        offer_details = item_details.get("offers")
+        #  Process offer on item if any
+        if offer_details:
+            # Split out offer details
+            offer = offer_details.split(" ")
+            offer_quantity = int(offer[0][0])
+            offer_price = int(offer[2])
+            if item not in offer_check:
+                offer_check[item] = 1
+            else:
+                offer_check[item] += 1
+                # Process if quantity is up to offer quantity
+                if offer_check[item] == offer_quantity:
+                    total_checkout_value += offer_price
+                    del offer_check[item]
+        # Add each sku price to total checkout
+        else:
+            total_checkout_value += skus_dict[item]["price"]
+    
+    # Process items not up to offer
+    for offer in offer_check:
+        item = skus_dict[offer]
+        total_checkout_value += offer_check[offer] * item["price"]
+        
+    #  return total checkout
+    return total_checkout_value
+
+
+
