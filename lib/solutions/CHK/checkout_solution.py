6ac8fcce429937f4ddadee12153e189d59b8a382
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 16:33:29 +0000

    Fri Jul 14 16:33:29 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,17 +15,19 @@ skus_dict = {
 def apply_offer(item, item_quantity):
     offer_price = 0
     offer = skus_dict[item].split(" ")
-    new_price = offer[2]
+    offer_price = offer[2]
     offer_quantity = offer[0].split()[0]
     if item_quantity < offer_quantity:
-        return -1
+        return item_quantity * skus_dict[item]["price"]
     of, rm = divmod(item_quantity, offer_quantity)
-    offer_price = of * 
+    new_price = (of * offer_price) + (rm * skus_dict[item]["price"])
+    return new_price
     
     
 
 def checkout(skus):
-    # Initialize total checkout value to zero 
+    # Initialize total checkout value to zero
+    total_checkout_value = 0
     checkout_items = {}
     # loop through each skus to get their price
     for item in skus:
@@ -38,11 +40,12 @@ def checkout(skus):
         # Check for invalid entry and return -1
         except KeyError:
             return -1
-        offer = skus_dict.get("offers"):
-        if offers:
-            discouted_price = apply_offer(item)
+        offer = skus_dict.get("offers")
+        if offer:
+            discounted_price = apply_offer(item, checkout_items["quantity"])
             
         # Add each sku price to total checkout
-        total_checkout_value += skus_dict[item]
+        total_checkout_value += discounted_price
     #  return total checkout
     return total_checkout_value
+
