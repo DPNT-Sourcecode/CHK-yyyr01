7eeb67e4caa14e8f26e728d01d319bedff027499
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 15:59:32 +0000

    Fri Jul 14 15:59:32 UTC 2023

--- test/solution_tests/CHK/checkout.py
+++ test/solution_tests/CHK/checkout.py
@@ -4,8 +4,9 @@ import pytest
 
 class TestCheckout():
     def test_checkout_valid(self):
-        assert checkout_solution("A") == 50
+        assert checkout_solution.checkout("A") == 50
 
     def test_checkout_invalid(self):
-        assert checkout_solution("PA") == -1
+        assert checkout_solution.checkout("PA") == -1
+
 
