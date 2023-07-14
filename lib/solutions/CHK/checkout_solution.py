8afa3c173d8ad637a544f50614234cdb07f30dd0
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 15:47:32 +0000

    Fri Jul 14 15:47:32 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,9 +2,15 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+item_dicts = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
 def checkout(skus):
     # how is the sku string structured?
-    print(skus)
-    return -1
+    
+
 
 
