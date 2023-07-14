740cd5604211e8e2a544c1ca90464903d7803267
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 19:12:08 +0000

    Fri Jul 14 19:12:08 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -15,10 +15,11 @@ class TestCheckout():
 
     def test_checkout_valid_multiple_with_offer(self):
         assert checkout_solution.checkout("AAAAAAA") == 350
-        assert checkout_solution.checkout("ADA") == 11
+        assert checkout_solution.checkout("ADA") == 115
         assert checkout_solution.checkout("ABAB") == 145
         assert checkout_solution.checkout("AABAB") == 175
-        assert checkout_solution.checkout("AAAAAAA") == 350
+        assert checkout_solution.checkout("AAAAAAAA") == 350
+
 
 
 
