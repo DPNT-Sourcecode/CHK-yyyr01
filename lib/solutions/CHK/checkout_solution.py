85d3e5dadb1a10179988946746ff3fb001ef71a6
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 19:36:08 +0000

    Fri Jul 14 19:36:08 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,7 +18,7 @@ skus_dict = {
 def checkout(skus):
     # Initialize total checkout value to zero
     total_checkout_value = 0
-    #  Store items with offers and quantity purchased
+    #  Store items and quantity purchased
     item_details = {}
     # loop through each skus to get their associated quantity
     for item in skus:
@@ -27,11 +27,13 @@ def checkout(skus):
             skus_dict[item]
         except KeyError:
             return -1
+        #  increase item quantity
         if item not in item_details:
             item_details[item] = 1
         else:
             item_details[item] += 1
     
+    # Calculate free offer on B after purchase of 2E
     quantity_of_E = item_details.get("E", 0)
     quantity_of_B = item_details.get("B", 0)
     if quantity_of_E >= 2:
@@ -39,10 +41,10 @@ def checkout(skus):
         if quantity_of_B >= offer:
                 item_details["B"] = item_details["B"] - offer
 
-
+    # Calculate total checkout value
     for item in item_details:
+        offer_price = 0
         if item == "A":
-            offer_price = 0
             quantity = item_details[item]
             if quantity >= 5:
                 offer, leftover = divmod(quantity, 5)
@@ -57,9 +59,8 @@ def checkout(skus):
                 offer_price += (offer * 130) + (leftover * skus_dict[item]["price"])
             else:
                 offer_price = item_details[item] * skus_dict[item]["price"]
-            total_ptotal_checkout_valueice += offer_price
+            total_checkout_value += offer_price
         elif item == "B":
-            offer_price = 0
             quantity = item_details[item]
             if quantity >= 2:
                 offer, leftover = divmod(quantity, 2)
@@ -114,5 +115,6 @@ def checkout(skus):
         
     # #  return total checkout
     # return total_checkout_value
+
 
 
--- test/solution_tests/CHK/checkout.py
+++ test/solution_tests/CHK/checkout.py
@@ -10,12 +10,21 @@ class TestCheckout():
         assert checkout_solution.checkout("PA") == -1
 
     def test_checkout_valid_multiple_without_offer(self):
-        assert checkout_solution.checkout("AD") == 100
+        assert checkout_solution.checkout("AA") == 100
         assert checkout_solution.checkout("ADA") == 115
 
     def test_checkout_valid_multiple_with_offer(self):
-        assert checkout_solution.checkout("AAA") == 130
+        assert checkout_solution.checkout("AAAAAAA") == 350
         assert checkout_solution.checkout("ADA") == 115
         assert checkout_solution.checkout("ABAB") == 145
         assert checkout_solution.checkout("AABAB") == 175
+        assert checkout_solution.checkout("AAAAAAAA") == 330
+
+    def test_checkout_get_free_B_with_offer(self):
+        assert checkout_solution.checkout("EEB") == 80
+        assert checkout_solution.checkout("EEBB") == 110
+        assert checkout_solution.checkout("EABB") == 135
+        assert checkout_solution.checkout("EBB") == 85
+        assert checkout_solution.checkout("EABD") == 135
+
 
