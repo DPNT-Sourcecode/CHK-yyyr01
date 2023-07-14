fe0080043ea82367a98474b68ceff8de0797948c
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-14 21:29:15 +0000

    Fri Jul 14 21:29:15 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 
 # Dictionary to store SKUs, prices and offers
 skus_dict = {
-    "A": { "price": 50, "offer": {"discount": [(5, 200), (3, 120)]}},
+    "A": { "price": 50, "offer": {"discount": [(5, 200), (3, 130)]}},
     "B": { "price": 30, "offer": {"discount": [(2, 45)]}},
     "C": { "price": 20},
     "D": { "price": 15},
@@ -20,6 +20,8 @@ def get_double_offer_price(item, quantity):
     highest_discount_price = item_discount_price[0][1]
     least_discount = item_discount_price[1][0]
     least_discount_price = item_discount_price[1][1]
+    print(highest_discount, highest_discount_price)
+    print(least_discount, least_discount_price)
     if quantity >= highest_discount:
         offer, leftover = divmod(quantity, highest_discount)
         offer_price += offer * highest_discount_price
@@ -45,6 +47,19 @@ def calculate_item_price(item, item_details):
     if item == "A":
         quantity = item_details[item]
         offer_price = get_double_offer_price("A", quantity)
+        # if quantity >= 5:
+        #     offer, leftover = divmod(quantity, 5)
+        #     offer_price += offer * 200
+        #     if leftover >= 3:
+        #         offer, leftover = divmod(leftover, 3)
+        #         offer_price += (offer * 130) + leftover * skus_dict[item]["price"]
+        #     else:
+        #         offer_price += leftover * skus_dict[item]["price"]
+        # elif quantity >= 3:
+        #     offer, leftover = divmod(quantity, 3)
+        #     offer_price += (offer * 130) + (leftover * skus_dict[item]["price"])
+        # else:
+        #     offer_price = item_details[item] * skus_dict[item]["price"]
         total_checkout_value += offer_price
     elif item == "B":
         quantity = item_details[item]
@@ -103,6 +118,7 @@ def checkout(skus):
             total_checkout_value += item_details[item] * skus_dict[item]["price"]
     
     return total_checkout_value
+
 
 
 
