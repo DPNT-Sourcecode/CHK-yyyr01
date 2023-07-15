398aef8975e58b2bfb78b48d2587540a0ae05f2f
sbx_user1051 <sbx_user1051@169.254.23.141>
2023-07-15 20:15:23 +0000

    Sat Jul 15 20:15:23 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -165,9 +165,9 @@ def checkout(skus):
         else:
             for_three.append(item)
             print(item,skus_dict[item]["price"] )
-            insort(for_three_sd, {item: skus_dict[item]["price"]}, key=lambda x: x[item])
+            for_three_sd.append({item: skus_dict[item]["price"]})
     print(for_three_sd, "k")
-
+    sorted_three = reversed(sorted)
     any_three_offer_price = get_any_three_offer_total_price(for_three)
     # Calculate free offer on B after purchase of 2E
     item_details = update_checkout_with_free_offers(item_details)
@@ -186,6 +186,7 @@ def checkout(skus):
             total_checkout_value += item_details[item] * skus_dict[item]["price"]
     
     return total_checkout_value + any_three_offer_price
+
 
 
 
