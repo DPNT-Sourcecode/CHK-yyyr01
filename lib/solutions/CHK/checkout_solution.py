877b4c16b772b0b7269e821fbfa257e9abbcdfd8
sbx_user1051 <sbx_user1051@169.254.24.37>
2023-07-15 00:43:57 +0000

    Sat Jul 15 00:43:57 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -22,14 +22,14 @@ skus_dict = {
     "P": { "price": 50, "offer": {"discount": [(5, 200)]}},
     "Q": { "price": 30, "offer": {"discount": [(3, 80)]}},
     "R": { "price": 50, "offer": {"free": [(3, "Q")]}},
-    "S": { "price": 20, "offer": {"any": [(3, "STXYZ", 45)]}},
-    "T": { "price": 20, "offer": {"any": [(3, "STXYZ", 45)]}},
+    "S": { "price": 20, "offer": {"any_group_items": [(3, 45)]}},
+    "T": { "price": 20, "offer": {"any_group_items": [(3, 45)]}},
     "U": { "price": 40, "offer": {"free": [(3, "U")]}},
     "V": { "price": 50, "offer": {"discount": [(2, 90), (3, 130)]}},
     "W": { "price": 20},
-    "X": { "price": 17, "offer": {"any": [(3, "STXYZ", 45)]}},
-    "Y": { "price": 20, "offer": {"any": [(3, "STXYZ", 45)]}},
-    "Z": { "price": 21, "offer": {"any": [(3, "STXYZ", 45)]}},
+    "X": { "price": 17, "offer": {"any_group_items": [(3, 45)]}},
+    "Y": { "price": 20, "offer": {"any_group_items": [(3, 45)]}},
+    "Z": { "price": 21, "offer": {"any_group_items": [(3, 45)]}},
 }
 
 double_discount_items = ["A", "H", "V"]
@@ -80,7 +80,7 @@ def get_any_three_offer_price(item, quantity):
 def calculate_item_price(item, item_details):
     offer_price = 0
     total_checkout_value = 0
-    any_three_offer_quantity
+    any_three_offer_quantity = 0
     if item in double_discount_items:
         quantity = item_details[item]
         offer_price = get_double_offer_price(item, quantity)
@@ -91,7 +91,9 @@ def calculate_item_price(item, item_details):
         total_checkout_value += offer_price
     elif item in any_three_offer:
         quantity = item_details[item]
-        offer_price = get_any_three_offer_price(item, quantity)
+        any_three_offer_quantity += quantity
+        if any_three_offer_quantity == 3:
+            offer_price = get_any_three_offer_price(item, quantity)
         total_checkout_value += offer_price
     return total_checkout_value
 
@@ -153,5 +155,6 @@ def checkout(skus):
             total_checkout_value += item_details[item] * skus_dict[item]["price"]
     
     return total_checkout_value
+
 
 
